685c73e38c7b823d3e388814430066d9a030a368
Igor Savynskyi <igor.savynskyi@just-eat.com>
2020-12-14 19:02:56 +0200

    Kuchi app register view initial development

--- 07-controls-and-user-input/projects/starter/Sources/App/Welcome/RegisterView.swift
+++ 07-controls-and-user-input/projects/starter/Sources/App/Welcome/RegisterView.swift
@@ -32,17 +32,33 @@
 
 import SwiftUI
 
-struct WelcomeView: View {
+struct RegisterView: View {
+    @State var name: String = ""
+    
     var body: some View {
-        ZStack {
-            WelcomeBackgroundImage()
+        VStack {
+            Spacer()
             WelcomeMessageView()
+            TextField("Type your name...", text: $name)
+                .padding(EdgeInsets(top: 8, leading: 16,
+                                    bottom: 8, trailing: 16))
+                .background(Color.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.blue)
+                )
+                .shadow(color: Color.gray.opacity(0.4),
+                        radius: 3, x: 1, y: 2)
+            Spacer()
         }
+        .padding()
+        .background(WelcomeBackgroundImage())
     }
 }
 
-struct WelcomeView_Previews: PreviewProvider {
+struct RegisterView_Previews: PreviewProvider {
     static var previews: some View {
-        WelcomeView()
+        RegisterView()
     }
 }
